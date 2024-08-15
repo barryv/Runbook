9c96dc78577c12536eda96376f531d0e6b8f8be2
barryv <barry.vaught@gmail.com>
2024-08-15 12:11:59 -0400

    Add files via upload

--- CutoverV2/TaskCustom.py
+++ CutoverV2/TaskCustom.py
@@ -4,33 +4,83 @@ import pandas as pd
 class TaskCustom:
         
     def set_taskcustom_columns(self):
-   
-        columns = ['Runbook Id', 'Task Id', 'Pid', 'Name', 'Value']
+        columns = ['Runbook Id', 'Task Id', 'Tool', 'Task', 'Action', 'IStatus', 'PStatus',
+                   'BStatus', 'GStatus', 'Requested', 'Coordinated', 'Job', 'Path',
+                  'Host', 'Env', 'CRQ', 'INC', 'Ratio', 'TID']
         
         return pd.DataFrame(columns=columns)
         
     def process_task_custom(self, rbid: str, item: list, df: pd.DataFrame):
        
-        pid=0
-        if ('attributes' in item and 
-           'custom_field_values' in item['attributes'] and
-           not item['attributes']['custom_field_values']):
+        
+        if item['attributes']['custom_field_values'] != {}:
            customFields = item['attributes']['custom_field_values']
-           for field in customFields: 
-               df = self.append_taskcustom_data(rbid, item['id'], str(pid), field, df) 
+           for field in customFields:
+               df = self.append_taskcustom_data(rbid, item['id'], field, df) 
         
         return df
         
-    def append_taskcustom_data(self, rbid: str, tid: str, pid: str, field: list, df: pd.DataFrame):
-            
-           
+    def append_taskcustom_data(self, rbid: str, tid: str, field: list, df: pd.DataFrame):
+        
+
         newRow = {
-            'Runbook Id': rbid,'
+            'Runbook Id': rbid,
             'Task Id': tid, 
-            'Pid': pid,
-            'Name': field['name'], 
-            'Value': field['value']
-            }
+            'Tool': '',
+            'Task': '',
+            'Action': '',
+            'IStatus': '',
+            'PStatus': '',
+            'BStatus': '',
+            'GStatus': '',
+            'Requested': '',
+            'Coordinated': '',
+            'Job': '',
+            'Path': '',
+            'Host': '',
+            'Env': '',
+            'CRQ': '',
+            'INC': '',
+            'Ratio': '',
+            'TID': ''
+        }
+        
+        if field['name'] == '[AOBlade]TaskTechnologyTool':
+            newRow['Tool'] = field['value']
+        elif field['name'] == '[AOBlade]TaskName':
+            newRow['Task'] = field['value']
+        elif field['name'] == '[AOSearchlight]Action':
+            newRow['Action'] = field['value']
+        elif field['name'] == '[Integration]Status':
+            newRow['IStatus'] = field['value']
+        elif field['name'] == 'Polling status':
+            newRow['PStatus'] = field['value']
+        elif field['name'] == 'Orchestrator via Polling: Orchestrator - BladeLogic: Polling status':
+            newRow['BStatus'] = field['value']
+        elif field['name'] == 'Orchestrator via Polling: Orchestrator - Greenlight: Polling status':
+            newRow['GStatus'] = field['value']
+        elif field['name'] == '[AOBlade]RequestedBy':
+            newRow['Requested'] = field['value']
+        elif field['name'] == '[AOBlade]CoordinatedBy':
+            newRow['Coordinated'] = field['value']
+        elif field['name'] == 'AOBlade]JobName':
+            newRow['Job'] = field['value']
+        elif field['name'] == '[AOBlade]JobPath':
+            newRow['Path'] = field['value']
+        elif field['name'] == '[AOBlade]HostName':
+            newRow['Host'] = field['value']
+        elif field['name'] == '[AOBlade]Environment':
+            newRow['Env'] = field['value']
+        elif field['name'] == '[AOBlade]CRQID':
+            newRow['CRQ'] = field['value']
+        elif field['name'] == '[AOBlade]INCID':
+            newRow['INC'] = field['value']
+        elif field['name'] == '[AOSearchlight]Ratio':
+            newRow['Ratio'] = field['value']
+        elif field['name'] == '[AOAnsible]TemplateID':
+            newRow['TID'] = field['value']
+
+
        
         return pd.concat([df, pd.DataFrame([newRow])], ignore_index=True)
 
